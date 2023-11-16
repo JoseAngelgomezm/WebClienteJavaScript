28eab511006c92ff668cd27745f8e81e41515277
Jose Angel <joseangelgmj@hotmail.com>
2023-11-16 12:45:39 +0100

    termina tutorial

--- React/mi-primera-app/src/index.js
+++ React/mi-primera-app/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null,
+    }
+  }
   render() {
     return (
-      <button className="square" onClick={()=> console.log("Click "+ this.props.value)}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {/* TODO */}
         {this.props.value}
       </button>
@@ -14,8 +20,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
