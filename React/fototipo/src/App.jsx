25a8c8a8e7850b4ea99168d39300e675f42aca40
Jose Angel <joseangelgmj@hotmail.com>
2024-02-07 12:46:39 +0100

    agrega funcionalidad

--- React/fototipo/src/App.jsx
+++ React/fototipo/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
-import datosPreguntas from './resources/datosPreguntas.json' 
+import datosPreguntas from './resources/datosPreguntas.json'
 import Pregunta from './components/Pregunta.jsx'
+import "./App.css"
+
+
+const MostrarPreguntas = (props) => {
+  const preguntas = props.jsonPreguntas.map((elemento, indice) => <div class="pregunta"><Pregunta puntuacion={(valor) => props.cambiarPuntuacion(valor)} key={indice} pregunta={elemento.pregunta} valores={elemento.valor} arrayRespuestas={elemento.respuestas}></Pregunta></div>)
+  return preguntas
+}
+
 function App() {
 
   const jsonPreguntas = datosPreguntas.preguntas
+  const [puntuacion, setPuntuacion] = useState(Array(datosPreguntas.preguntas.length).fill(-1));
+
+
+  const cambiarPuntuacion = (valor, numeroPregunta) => {
+    console.log(valor, indice)
+  }
 
   return (
     <>
-    {jsonPreguntas.map((elemento) => <Pregunta key="" pregunta={elemento.pregunta} valores={elemento.valor} arrayRespuestas={elemento.respuestas}></Pregunta>)}
+      <header>
+        <h2>Descubre tu FOTOTIPO</h2>
+        <p>Responde al siguiente cuestrionario y podras saber tu fototipo segun una puntuacion de cada respuesta</p>
+      </header>
+      <div id="preguntas">
+      <MostrarPreguntas cambiarPuntuacion={(valor) => cambiarPuntuacion(valor)} jsonPreguntas={jsonPreguntas}></MostrarPreguntas>
+      <button id ="enviar">Enviar Preguntas</button>
+      </div>
     </>
   )
 }
--- React/fototipo/src/components/Pregunta.jsx
+++ React/fototipo/src/components/Pregunta.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react"
+
+
 export default function Pregunta(props) {
+    const [botonPulsado, setBotonPulsado] = useState()
+    
+
+    const botones = props.arrayRespuestas.map((elemento, indice) => {
+        if (botonPulsado === props.valores[indice]) {
+            return <button disabled key={indice}
+                onClick={() => {
+                    setBotonPulsado(props.valores[indice]);
+                    props.puntuacion(props.valores[indice])
+                }}
+                value={props.valores[indice]}>{elemento}</button>
+        }else{
+            return <button key={indice}
+            onClick={() => {
+                setBotonPulsado(props.valores[indice]);
+                props.puntuacion(props.valores[indice])
+            }}
+            value={props.valores[indice]}>{elemento}</button>
+        }
+    })
 
     return (
         <>
-        <h3>{props.pregunta}</h3>
-        {props.arrayRespuestas.map((elemento, indice) => <button value={props.valores[indice]}>{elemento}</button>)}
+            <h3>{props.pregunta}</h3>
+
+            {botones}
         </>
     )
 }
